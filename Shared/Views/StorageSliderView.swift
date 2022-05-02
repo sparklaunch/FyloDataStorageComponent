5156d5cad1490e7a8d501e41ea952f14f7892d7a
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-02 14:26:52 +0900

    Add the storage slider texts.

--- Shared/Views/StorageSliderView.swift
+++ Shared/Views/StorageSliderView.swift
@@ -9,23 +9,36 @@ import SwiftUI
 
 struct StorageSliderView: View {
     var body: some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .leading) {
-                Capsule(style: .continuous)
-                    .frame(height: 30)
-                Capsule(style: .continuous)
-                    .fill(LinearGradient(colors: [Color("LeadingGradientColor"), Color("TrailingGradientColor")], startPoint: .leading, endPoint: .trailing))
-                    .frame(width: geometry.size.width * 0.815, height: 20)
-                    .padding(4)
-                    .overlay(
-                        HStack {
-                            Spacer()
-                            Circle()
-                                .fill(Color.white)
-                                .frame(width: 16, height: 16)
-                        }
-                            .padding(8)
-                    )
+        VStack(spacing: 32) {
+            GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    Capsule(style: .continuous)
+                        .frame(height: 30)
+                    Capsule(style: .continuous)
+                        .fill(LinearGradient(colors: [Color("LeadingGradientColor"), Color("TrailingGradientColor")], startPoint: .leading, endPoint: .trailing))
+                        .frame(width: geometry.size.width * 0.815, height: 20)
+                        .padding(4)
+                        .overlay(
+                            HStack {
+                                Spacer()
+                                Circle()
+                                    .fill(Color.white)
+                                    .frame(width: 16, height: 16)
+                            }
+                                .padding(8)
+                        )
+                }
+            }
+            HStack {
+                Text("0 GB")
+                    .font(.footnote)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("TextColor"))
+                Spacer()
+                Text("1000 GB")
+                    .font(.footnote)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("TextColor"))
             }
         }
     }
