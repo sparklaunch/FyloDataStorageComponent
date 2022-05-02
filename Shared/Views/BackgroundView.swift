524f0abd50cb27a558e62c6b571cba0389edfbe4
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-02 11:26:28 +0900

    Add the background view.

---
+++ Shared/Views/BackgroundView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundView.swift
+//  Fylo Data Storage Component (iOS)
+//
+//  Created by Jinwook Kim on 2022/05/02.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Image(decorative: "Background")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+            .scaledToFill()
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
+            .previewLayout(.sizeThatFits)
+    }
+}
